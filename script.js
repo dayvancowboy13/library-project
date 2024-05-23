66251443254427f8b674c1cb6b9c2d09b5002560
dayvancowboy13 <chris.e.friedrich@gmail.com>
2024-05-22 20:51:40 -0700

    started project

---
+++ script.js
@@ -0,0 +1,15 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addToLibrary() {
+    
+}
+// Write a function that loops through the array and displays each book on the page. 
+// You can display them in some sort of table, or each on their own “card”. 
+// It might help for now to manually add a few books to your array so you can see the display.
