5a8f2d5a0d2db608456593a02d8850e8d473aa7d
dayvancowboy13 <chris.e.friedrich@gmail.com>
2024-07-07 10:31:14 -0700

    added form validation functionality

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const cardContainer = document.querySelector('.card-container');
 const addButton = document.querySelector(".add-book");
 
 class Book {
-    constructor (title,author,pages, read){
+    constructor(title, author, pages, read) {
         this.title = title;
         this.author = author;
         this.pages = pages;
@@ -13,35 +13,18 @@ class Book {
     }
 }
 
-class Library{
-    constructor(){
-        this.bookArray = [];    
+class Library {
+    constructor() {
+        this.bookArray = [];
     }
 
-    get books(){
+    get books() {
         return this.bookArray;
     }
 
-    addBookToLibrary(){
-
-        const inputTitle = document.querySelector('#book_title');
-        const inputAuthor = document.querySelector('#book_author');
-        const inputPages = document.querySelector('#book_pages');
-        const inputRead = document.querySelector('#has_read');
-
-        if(inputTitle.value.length === 0 ||
-            inputAuthor.value.length === 0 ||
-            inputPages.value.length === 0) {
-                alert("Please enter value input!");
-        } else {
-            const book = new Book(inputTitle.value, inputAuthor.value, inputPages.value, inputRead.value);
-            
-                inputTitle.value = '';
-                inputAuthor.value = '';
-                inputPages.value = null;
-            
-                this.bookArray.push(book);
-        }
+    addBookToLibrary(title, author, pages, read) {
+        const book = new Book(title, author, pages, read);
+        this.bookArray.push(book);
     }
 
     removeCard(bookCard) {
@@ -52,11 +35,11 @@ class Library{
         console.log(`My book array is now length ${this.bookArray.length}`);
     }
 
-    createBookCard(book){
+    createBookCard(book) {
         console.log(this.bookArray)
         const bookCard = document.createElement("div");
         bookCard.className = "book-card";
-    
+
         const title = document.createElement("h1");
         title.textContent = book.title;
         const author = document.createElement("h2");
@@ -64,18 +47,18 @@ class Library{
         const pages = document.createElement("h3");
         pages.textContent = book.pages + " pages";
         const readButton = document.createElement("button");
-    
-        if(book.read === 'yes'){
+
+        if (book.read === 'yes') {
             readButton.textContent = "Read it!"
             readButton.className = "has-read";
         } else {
             readButton.textContent = "Not read";
             readButton.className = "not-read";
         }
-    
-        readButton.addEventListener('click', ()=>{
+
+        readButton.addEventListener('click', () => {
             let index = this.bookArray.findIndex(x => x.title === book.title)
-            if(readButton.className === "has-read"){
+            if (readButton.className === "has-read") {
                 readButton.className = 'not-read';
                 readButton.textContent = "Not read";
                 this.bookArray[index].read = 'no';
@@ -85,27 +68,27 @@ class Library{
                 this.bookArray[index].read = 'yes';
             }
         });
-        
+
         const delButton = document.createElement('button');
         delButton.className = "delete-card";
-        delButton.addEventListener('click', ()=>{
+        delButton.addEventListener('click', () => {
             this.removeCard(delButton.parentNode);
         })
-        
-    
+
+
         bookCard.appendChild(title);
         bookCard.appendChild(author);
         bookCard.appendChild(pages);
         bookCard.appendChild(readButton);
         bookCard.appendChild(delButton);
-        
+
         this.appendToContainer(bookCard);
     }
 
-    appendToContainer(bookCard){   
+    appendToContainer(bookCard) {
         cardContainer.appendChild(bookCard);
     }
-    
+
 }
 
 const myLibrary = new Library();
@@ -118,13 +101,24 @@ addButton.addEventListener('click', () => {
 dialogClose.addEventListener('click', () => {
     dialog.close();
 });
-dialogAddBook.addEventListener('click', () => {
-    myLibrary.addBookToLibrary();
 
-    while(cardContainer.childElementCount != 0){
+const titleInput = document.getElementById('book_title');
+const authorInput = document.getElementById('book_author');
+const pagesInput = document.getElementById('book_pages');
+const readInput = document.getElementById('has_read');
+const form = document.querySelector('.new-book-form');
+
+form.addEventListener('submit', function () {
+    console.log('submitting')
+    myLibrary.addBookToLibrary(titleInput.value, authorInput.value, pagesInput.value, readInput.value);
+
+    while (cardContainer.childElementCount != 0) {
         cardContainer.removeChild(cardContainer.lastChild)
     }
     myLibrary.books.forEach((book) => myLibrary.createBookCard(book));
 
-    dialog.close();
+    // clear the form inputs
+    titleInput.value = '';
+    authorInput.value = '';
+    pagesInput.value = null;
 });
